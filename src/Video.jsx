c8d976a10ff95293bfd6f002ddf5f96658bdac11
Alimalone <maeva.alice.carras@gmail.com>
2020-11-26 16:16:41 +0100

    video

---
+++ src/Video.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import videospace from "./video/video.mp4";
+
+export default function Video() {
+    return (
+        <div className="video">
+            <video src={videospace} autoplay="true"/>
+        </div>
+
+    );
+
+
+
+
+
+
+
+
+}
+
+
